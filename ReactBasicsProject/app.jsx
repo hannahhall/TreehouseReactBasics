1aadb4f098dc5d4f73bc868a837c884226c4faaa
Hannah Hall <hannahkhall13@gmail.com>
2016-09-30 14:34:08 -0500

    players array and map function to iterate over each

--- ReactBasicsProject/app.jsx
+++ ReactBasicsProject/app.jsx
@@ -1,13 +1,27 @@
+var PLAYERS = [
+  {
+    name: "Mick Jagger",
+    score: 11,
+    id: 1
+  },
+  {
+    name: "Keith Richards",
+    score: 21,
+    id: 2
+  }
+]
+
 function Header (props) {
   return (
     <div className="header">
       <h1>{props.title}</h1>
     </div>
   )
-}
+};
+
 Header.propTypes = {
   title: React.PropTypes.string.isRequired
-}
+};
 
 function Counter (props) {
   return (
@@ -17,10 +31,11 @@ function Counter (props) {
       <button className="counter-action increment"> + </button>
     </div>
   )
-}
+};
+
 Counter.propTypes = {
-  score: React.PropTypes.string.isRequired
-}
+  score: React.PropTypes.number.isRequired
+};
 
 function Player (props) {
   return (
@@ -31,27 +46,37 @@ function Player (props) {
       </div>
     </div>
   )
-}
+};
+
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.string.isRequired
-}
+  score: React.PropTypes.number.isRequired
+};
+
 function Application (props) {
   return (
     <div className="scoreboard">
       <Header title={props.title}/>
       <div className="players">
-        <Player name="Mick Jagger" score="11" />
-        <Player name="Keith Richards" score="21" />
+        {props.players.map(function (player) {
+          return <Player name={player.name} score={player.score} key={player.id} />
+        })}
       </div>
     </div>
   );
-}
+};
+
 Application.propTypes = {
   title: React.PropTypes.string,
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired
+  })).isRequired
 };
 
 Application.defaultProps = {
   title: "Scoreboard"
 }
-ReactDOM.render(<Application />, document.getElementById('container'));
+
+ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
