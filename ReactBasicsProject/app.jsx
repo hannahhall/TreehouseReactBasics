e91753ef76fbf7d29dd21d2013fb8a62d0d07e23
Hannah Hall <hannahkhall13@gmail.com>
2016-09-30 13:57:00 -0500

    static scoreboard

--- ReactBasicsProject/app.jsx
+++ ReactBasicsProject/app.jsx
@@ -1,8 +1,21 @@
 function Application () {
   return (
-    <div>
-      <h1>Hello</h1>
-      <p>Rendered from App component</p>
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">Mick Jagger</div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">10</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
