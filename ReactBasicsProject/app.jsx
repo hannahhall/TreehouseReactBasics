926a2c39e196dbb249e7a3b3a39a31728378aa42
Hannah Hall <hannahkhall13@gmail.com>
2016-09-30 14:20:15 -0500

    broke up Application into smaller components

--- ReactBasicsProject/app.jsx
+++ ReactBasicsProject/app.jsx
@@ -1,20 +1,48 @@
+function Header (props) {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
+}
+
+function Counter (props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score">{props.score}</div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
+}
+Counter.propTypes = {
+  score: React.PropTypes.string.isRequired
+}
+
+function Player (props) {
+  return (
+    <div className="player">
+      <div className="player-name">{props.name}</div>
+      <div className="player-score">
+        <Counter score={props.score} />
+      </div>
+    </div>
+  )
+}
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.string.isRequired
+}
 function Application (props) {
   return (
     <div className="scoreboard">
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
+      <Header title={props.title}/>
       <div className="players">
-        <div className="player">
-          <div className="player-name">Mick Jagger</div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score">10</div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
+        <Player name="Mick Jagger" score="11" />
+        <Player name="Keith Richards" score="21" />
       </div>
     </div>
   );
