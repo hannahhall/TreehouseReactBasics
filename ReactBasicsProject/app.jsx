c2a9a4caef85d4b54c4adcbef8079ba6a89373d3
Hannah Hall <hannahkhall13@gmail.com>
2016-10-05 18:57:11 -0500

    state and scoring

--- ReactBasicsProject/app.jsx
+++ ReactBasicsProject/app.jsx
@@ -23,26 +23,42 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired
 };
 
-function Counter (props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score">{props.score}</div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  )
-};
+const Counter = React.createClass({
+  propTypes: {
+  },
+  getInitialState: function () {
+    return {
+      score: 0,
 
-Counter.propTypes = {
-  score: React.PropTypes.number.isRequired
-};
+    }
+  },
+  incrementScore: function (e) {
+    this.setState({
+      score: (this.state.score + 1),
+    })
+  },
+  decrementScore: function (e) {
+    this.setState({
+      score: (this.state.score - 1),
+    })
+  },
+  render: function () {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score">{this.state.score}</div>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    )
+  }
+})
 
 function Player (props) {
   return (
     <div className="player">
       <div className="player-name">{props.name}</div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter />
       </div>
     </div>
   )
